83a70c29c5ebc0c486ac4037edfb1481cf2bd6fb
Patrick Radius <patrick.radius@coolblue.nl>
2024-06-14 17:52:12 +0200

    Removed default list

--- service/VoteService.ts
+++ service/VoteService.ts
@@ -6,15 +6,7 @@ export type Vote = {
     vote: number
 }
 export class VoteService {
-    private votes: Vote[] =  [
-        {id: "1", name: 'Jasmin', vote: 1, user_id: 'jasmin'},
-        {id: "2", name: 'Mihiran', vote: 3, user_id: 'mihiran'},
-        {id: "3", name: 'Nejat', vote: 5, user_id: 'nejat'},
-        {id: "4", name: 'Patrick', vote: 21, user_id: 'patrick'},
-        {id: "5", name: 'Sebastian', vote: 5, user_id: 'sebastian'},
-        {id: "6", name: 'Shakeel', vote: 3, user_id: 'shakeel'},
-        {id: "7", name: 'Victor', vote: 0, user_id: 'victor'},
-    ] ;
+    private votes: Vote[] = [];
     getVotes() {
         return this.votes;
     }
